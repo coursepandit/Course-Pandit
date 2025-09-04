137fd1c6ddb9c6e873335894f4c5443ff63392bd
Course Pandit <tradesbandb@gmail.com>
2025-09-04 13:28:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,14 +1,14 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#CoursePandit
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21589142"))
-API_HASH = environ.get("API_HASH", "f99db4b15e3179c794a058b21b4ad8546")
-BOT_TOKEN = environ.get("BOT_TOKEN", "83429415437:AAEnlmYUJkABCL_ucegkVmGfDsbwiqQg")
+API_ID = int(environ.get("API_ID", "24724813"))
+API_HASH = environ.get("API_HASH", "78087dcee4bafeeb5936a8cecaee5225")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8438727886:AAEDOJ8mFUpb9VDKILeFo4kFlM8tvPX8dp0")
 
-OWNER = int(environ.get("OWNER", "80346846315"))
-CREDIT = environ.get("CREDIT", "[〱⏤͟͞𝙃 𝙈🐦‍🔥 〄")
+OWNER = int(environ.get("OWNER", "7875250107"))
+CREDIT = environ.get("CREDIT", "CoursePanditBot")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
